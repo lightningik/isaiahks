f8de4d2af9713530f765b73dd0fe992a7cb4d580
lightningik <Izayikhan@gmail.com>
2023-10-27 13:57:03 -0700

    Initial Commit

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+      <div className="flex justify-center bg-card-bg rounded-lg m-5 h-[95vh]">
+          <h1 className="font-inter text-center bg-[linear-gradient(141deg,#cebbc9ff,#694873ff)] bg-clip-text text-[70px] relative inline-block m-auto">
+              Test
+              <script>
+              test
+          </script>
+          </h1>
+      </div>
+  )
+}
+let test = console.log("yrouete")
